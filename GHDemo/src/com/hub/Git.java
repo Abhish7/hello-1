8dbd48eda7c498f52a568eb047915f2db839da4b
Abhish7 <abhishberad@gmail.com>
2020-06-02 23:03:00 +0530

    Welcome line added in Git.java file

--- GHDemo/src/com/hub/Git.java
+++ GHDemo/src/com/hub/Git.java
@@ -3,7 +3,8 @@ package com.hub;
 public class Git {
 
 	public static void main(String[] args) {
-System.out.println("Hello GitHub");
+	System.out.println("Hello GitHub");
+	System.out.println("Welcome to new world");
 	}
 
 }
